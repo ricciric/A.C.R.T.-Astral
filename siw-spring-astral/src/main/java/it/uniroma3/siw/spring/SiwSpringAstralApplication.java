bbea2d7c97d01500ff5d2151f56cbd605aef4583
ricciric <riki.marasca@gmail.com>
2021-06-11 11:09:43 +0200

    Creazione progetto spring

---
+++ siw-spring-astral/src/main/java/it/uniroma3/siw/spring/SiwSpringAstralApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwSpringAstralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwSpringAstralApplication.class, args);
+	}
+
+}
