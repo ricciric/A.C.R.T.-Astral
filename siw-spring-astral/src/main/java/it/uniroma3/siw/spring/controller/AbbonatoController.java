9695ba2241f608971b8744ac1266a782da767a2f
ricciric <riki.marasca@gmail.com>
2021-06-14 12:00:47 +0200

    Abbonato
    Form abbonato
    Lista abbonati
    Registrazione avvenuta con successo

---
+++ siw-spring-astral/src/main/java/it/uniroma3/siw/spring/controller/AbbonatoController.java
@@ -0,0 +1,38 @@
+package it.uniroma3.siw.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.spring.model.Abbonato;
+import it.uniroma3.siw.spring.service.AbbonatoService;
+
+@Controller
+public class AbbonatoController {
+
+	@Autowired 
+	private AbbonatoService abbonatoService;
+	
+	@RequestMapping(value ="/newAbbonato", method = RequestMethod.GET)
+	public String newAbbonato(Model model) {
+		model.addAttribute("abbonato", new Abbonato());
+		return "abbonatoForm.html";
+	}
+	
+	@RequestMapping(value ="/abbonato", method = RequestMethod.GET)
+	public String saveAbbonato(@ModelAttribute("abbonato") Abbonato abbonato, Model model) {
+		this.abbonatoService.inserisci(abbonato);
+		model.addAttribute("abbonato", abbonato);
+		return "registrazioneSuccesso.html";
+	}
+	
+	@RequestMapping(value="/abbonati", method=RequestMethod.GET)
+	public String getAbbonati(Model model) {
+		model.addAttribute("abbonati", this.abbonatoService.tutti());
+		return "listaAbbonati.html";
+	}
+
+}
--- siw-spring-astral/src/main/java/it/uniroma3/siw/spring/service/AbbonatoService.java
+++ siw-spring-astral/src/main/java/it/uniroma3/siw/spring/service/AbbonatoService.java
@@ -1,12 +1,28 @@
 package it.uniroma3.siw.spring.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.spring.model.Abbonato;
+import it.uniroma3.siw.spring.repository.AbbonatoRepository;
 
 @Service
 public class AbbonatoService {
 
 	@Autowired
-	private AbbonatoService abbonatoService;
+	private AbbonatoRepository abbonatoRepository;
 
+	@Transactional
+	public void inserisci(Abbonato abbonato) {
+		this.abbonatoRepository.save(abbonato);
+	}
+	
+	@Transactional
+	public List<Abbonato> tutti(){
+		return (List<Abbonato>) this.abbonatoRepository.findAll();
+	}
 }
